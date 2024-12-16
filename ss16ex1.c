b69291fd220e75f78ae8cc278548ddb18ea48881
mthang24242424 <wunkevan@gmail.com>
2024-12-16 13:49:40 +0700

    ss16ex1

--- ss16ex1.c
+++ ss16ex1.c
@@ -3,6 +3,8 @@ int main(){
 	int x=5;
 	int *xPtr=&x ;
 	printf("dia chi ma con tro dang tro toi la: %lu\n",xPtr);
-	printf("dia chi cua bien x la: %lu",&x);
+	printf("dia chi cua bien x la: %lu\n",&x);
+	printf("gia tri cua con tro la: %d\n",*xPtr);
+	printf("gia tri cua bien x la: %d",x); 
 	return 0; 
 } 
