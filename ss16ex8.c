ca01bdaa08847512a73e3c18a7e31ab80c7f0bb5
mthang24242424 <wunkevan@gmail.com>
2024-12-16 13:41:55 +0700

    ss16ex8

--- ss16ex8.c
+++ ss16ex8.c
@@ -4,12 +4,12 @@ int main() {
     char inputString[100], reverseString[100];
     printf("nhap chuoi: ");
     fgets(inputString, sizeof(inputString), stdin);
-    reverseStringUsingPointer(inputString, reverseString);
+    daoNguoc(inputString, reverseString);
     printf("chuoi ban dau: %s\n", inputString);
     printf("chuoi dao nguoc: %s\n", reverseString);
     return 0;
 }
-void reverseStringUsingPointer(char *input, char *reverse) {
+void daoNguoc(char *input, char *reverse) {
     int length = strlen(input);
     char *start = input;
     char *end = input + length - 1;
